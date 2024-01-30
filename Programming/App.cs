2d9e5acd1bb9d51c582ff743fc05ea160eb2b759
manisha kumari <manishakumari.it00@gmail.com>
2024-01-30 12:52:07 +0530

    Add project files.

---
+++ Programming/App.cs
@@ -0,0 +1,28 @@
+﻿using HelloWorldLibrary.BusinessLogic;
+
+namespace Programming;
+
+public class App
+{
+    private readonly IMessages _messages;
+    public App(IMessages messages)
+    {
+        _messages = messages; 
+    }
+    //Programming.exe -lang=es
+    public void Run(string[] args)
+    {
+        string lang = "en";
+
+        for(int i=0; i<args.Length; i++)
+        {
+            if (args[i].ToLower().StartsWith(value: "-lang="))
+            {
+                lang = args[i].Substring(startIndex: 6);
+                break;
+            }
+        }
+        string message = _messages.Greeting(lang);
+        Console.WriteLine(message);
+    }
+}
